4e6c70f4789b62b892a8de36f9a78d82e56a182e
Kai Chen <kaichen@outlook.com>
2017-05-17 12:06:48 -0700

    Adding missing file.

--- cmd/util/util.go
+++ cmd/util/util.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"errors"
+	"strconv"
 	"strings"
 
 	"github.com/spf13/viper"
@@ -37,4 +38,69 @@ func ConfigCluster(cluster string) (string, error) {
 	}
 
 	return result, nil
+}
+
+// ParseListener parses the listener information specified in the form name:listenerPort:containerPort/protocol
+func ParseListener(desc string) (string, int, int, string, error) {
+	var name string
+	var listenerPort int
+	var containerPort int
+	var protocol string
+	var err error
+
+	result := strings.Split(desc, "/")
+
+	if len(result) > 2 {
+		return "", 0, 0, "", errors.New("invalid listener descriptor, expected [name:][listenerPort:]containerPort[/protocol]")
+	}
+
+	if len(result) == 2 {
+		desc = result[0]
+		protocol = result[1]
+
+		if protocol != "http" && protocol != "tcp" {
+			return "", 0, 0, "", errors.New("invalid protocol specified, supported protocols are [tcp, http]")
+		}
+	}
+
+	result = strings.Split(desc, ":")
+
+	if len(result) > 3 {
+		return "", 0, 0, "", errors.New("invalid listener descriptor, expected [name:][listenerPort:]containerPort")
+	}
+
+	switch len(result) {
+	case 1:
+		// containerPort
+		containerPort, err = strconv.Atoi(result[0])
+		if err != nil {
+			return "", 0, 0, "", errors.New("invalid listener descriptor, containerPort should be int")
+		}
+	case 2:
+		// name:containerPort or listenerPort:containerPort
+		containerPort, err = strconv.Atoi(result[1])
+		if err != nil {
+			return "", 0, 0, "", errors.New("invalid listener descriptor, expected name:containerPort or listenerPort:containerPort")
+		}
+
+		listenerPort, err = strconv.Atoi(result[0])
+		if err != nil {
+			name = result[0]
+		}
+	case 3:
+		// name:listenerPort:containerPort
+		name = result[0]
+
+		listenerPort, err = strconv.Atoi(result[1])
+		if err != nil {
+			return "", 0, 0, "", errors.New("invalid listener descriptor, listenerPort is not int, in name:listenerPort:containerPort")
+		}
+
+		containerPort, err = strconv.Atoi(result[2])
+		if err != nil {
+			return "", 0, 0, "", errors.New("invalid listener descriptor, containerPort is not int, in name:listenerPort:containerPort")
+		}
+	}
+
+	return name, listenerPort, containerPort, protocol, nil
 }
