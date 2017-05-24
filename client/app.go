d5a47305c5ce31c1ca963ea8f7181c6e9b88758c
Kai Chen <kaichen@outlook.com>
2017-05-24 01:11:05 -0700

    feature: Add service list to compose inspect.

--- client/app.go
+++ client/app.go
@@ -2,8 +2,9 @@ package client
 
 // App defines the response body of the InspectApp API.
 type App struct {
-	Name      string `json:"app_name"`
-	ID        string `json:"uuid"`
-	State     string `json:"current_status"`
-	CreatedBy string `json:"created_by"`
+	Name      string    `json:"app_name"`
+	ID        string    `json:"uuid"`
+	State     string    `json:"current_status"`
+	CreatedBy string    `json:"created_by"`
+	Services  []Service `json:"services"`
 }
